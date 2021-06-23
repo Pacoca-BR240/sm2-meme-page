446da6e1badcf6501ac4ec85acac9d92f39126a2
Pacoca-BR240 <79349328+Pacoca-BR240@users.noreply.github.com>
2021-06-23 14:41:27 -0300

    Add files via upload

---
+++ memepage.js
@@ -0,0 +1,20 @@
+function changeImage()
+{
+var img = document.getElementById("image");
+img.src="https://i.imgflip.com/5e7nrj.jpg";
+return false;
+}
+
+function revertImage()
+{
+var img = document.getElementById("image");
+img.src="https://i.imgflip.com/5e7nnp.jpg";
+return false;
+}
+
+function revertImage()
+{
+var img = document.getElementById("image");
+img.src="https://cdn.discordapp.com/attachments/857275252412383263/857279682353430548/ZomboMeme_23062021172242.jpg";
+return false;
+}
