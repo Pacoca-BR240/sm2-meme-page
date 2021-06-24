84ede3a15e141206656e340b3e438aab554afce2
Pacoca-BR240 <79349328+Pacoca-BR240@users.noreply.github.com>
2021-06-24 15:09:03 -0300

    Add files via upload

---
+++ screept.js
@@ -0,0 +1,16 @@
+// penis
+console.log("Hello mister peeping tom");
+
+var myAudio = document.getElementById("myAudio");
+var isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
