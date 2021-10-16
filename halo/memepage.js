f7b6d1199ee2539dbde0338c10c6a1b861d878b0
Pacoca-BR240 <79349328+Pacoca-BR240@users.noreply.github.com>
2021-10-16 07:01:00 -0300

    Add files via upload

---
+++ halo/memepage.js
@@ -0,0 +1,13 @@
+function changeImage()
+{
+var img = document.getElementById("image");
+img.src="https://i.imgflip.com/5e7nrj.jpg";
+return false;
+}
+
+function revertImage()
+{
+var img = document.getElementById("image");
+img.src="https://i.imgflip.com/5e7nnp.jpg";
+return false;
+}
